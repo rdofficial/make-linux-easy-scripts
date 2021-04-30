c63c002a4d80cab5b08951eae947263992d8fb7a
Rishav Das <61372472+rdofficial@users.noreply.github.com>
2021-04-30 15:09:19 +0530

    Added the script file 'check-package-manager.sh'
    In this commit, we have added the script file named check-package-manager.sh to the repository. The script serves the functions to determine the information of the package-manager installed on the linux distro (computer system) that the user is using currently. The script file is currently not filled with the required code, it only contains the commented documentation. The code will be updated soon.

---
+++ basic/check-package-manager.sh
@@ -0,0 +1,13 @@
+# check-package-manager [bash / shell script]
+#
+# This script serves the function of checking the package-manager software that your current linux distro uses, and then gives the output to the user with all the required information. The script contains the functions and code that does it. The same code is used in the different scripts in this repository for checking the package-manager of the current linux distro.
+#
+# Author : Rishav Das (https://github.com/rdofficial)
+# Created on : April 31, 2021
+#
+# Last modified : -
+# Authored by : -
+#
+# Authors contributed to this script (Add your name below if you have contributed) :
+# 1. Rishav Das (https://github.com/rdofficial/, email:rdofficial192@gmail.com)
+#
