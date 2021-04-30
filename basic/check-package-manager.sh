a1de8152dfb2e810e18f9bd4e4afa43a8e85c7ee
Rishav Das <61372472+rdofficial@users.noreply.github.com>
2021-04-30 15:52:54 +0530

    Added the code for recognizing the package-manager installed to 'check-package-manager.sh' script file
    In this commit, we added all the code required to determine that which package manager software is currently installed on the computer system (Linux distro). The script can currently only determine limited package managers and they are : APT, PACMAN, DNF, RPM, YUM, and ABS. These package managers are only chosen because they use themselves as default and makes it much more easy for the user to execute the command even without automating. Like for updating the linux system, we can use the command  'apt update' if we are using the APT package manager. Slowly as per time, the script will be modified enough such that it determines even more package managers.

--- basic/check-package-manager.sh
+++ basic/check-package-manager.sh
@@ -11,3 +11,51 @@
 # Authors contributed to this script (Add your name below if you have contributed) :
 # 1. Rishav Das (https://github.com/rdofficial/, email:rdofficial192@gmail.com)
 #
+
+# First checking for APT
+pmName=""
+if [[ -z $( type -p apt ) ]]; then
+	# If the package manager is not APT, then we continue for checking for pacman
+
+	if [[ -z $( type -p pacman ) ]]; then
+		# If the package manager is not pacman, then we continue to check for YUM
+
+		if [[ -z $( type -p yum ) ]]; then
+			# If the package manager is not YUM, then we continue to check for DNF
+
+			if [[ -z $( type -p dnf ) ]]; then
+				# If the package manager is not DNF, then we continue to check for the ABS
+
+				if [[ -z $( type -p abs ) ]]; then
+					# If the package manager is not ABS, then we declare that whatever package-manager that the operating system (Linux distro) uses is not supported by the scripts in this project / repository
+
+					echo -e "\033[07;91m[ Error : Unrecognized package manager. Whatever package manager is being used on this computer is not supported by any of the scripts of this project (repository). Please contact the author/owner for more information, or just look at the docs / help section ]\033[00m"
+					exit
+				else
+					# If the package manager is ABS
+
+					pmName="abs"
+				fi
+			else
+				# If the package manager is DNF
+
+				pmName="dnf"
+			fi
+		else
+			# If the package manager is YUM
+
+			pmName="yum"
+		fi
+	else
+		# If the package manager is pacman
+
+		pmName="pacman"
+	fi
+else
+	# If the package manager is APT
+
+	pmName="apt"
+fi
+
+# Finally displaying the name of the package manager on the console screen
+echo -e "The package manager used on this computer is : \033[92m${pmName}\033[00m"
