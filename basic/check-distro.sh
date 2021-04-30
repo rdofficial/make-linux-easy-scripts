ac75c1e7dbb2c3f5a581558b7775db244988df33
Rishav Das <61372472+rdofficial@users.noreply.github.com>
2021-04-30 15:19:26 +0530

    Added the script file named check-distro.sh
    In this commit, we have added the script file named  check-distro.sh to this repository / project. The script serves the functions to find out the information about the linux distribution that is currently installed in the user's computer system.  Although the script file currently contains the commented documentation and no code, but soon enough the file will be updated with error free and working code. ;-)

---
+++ basic/check-distro.sh
@@ -0,0 +1,13 @@
+# check-distro [bash / shell script]
+#
+# This script serves the functions to find out the information about the linux distribution that is currently installed in the user's computer system. The functions that are defined in this script are used by other scripts around this repository for getting information about the linux distro in which they are being executed.
+#
+# Author : Rishav Das (https://github.com/rdofficial)
+# Created on : April 31, 2021
+#
+# Last modified : -
+# Authored by : -
+#
+# Authors contributed to this script (Add your name below if you have contributed) :
+# 1. Rishav Das (https://github.com/rdofficial/, email:rdofficial192@gmail.com)
+#
