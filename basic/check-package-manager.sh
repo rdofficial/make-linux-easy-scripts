f4cf924213ee8564e7c21eed420b2308c3193fa1
Rishav Das <61372472+rdofficial@users.noreply.github.com>
2021-05-03 09:08:19 +0530

    Added the feature of specific argument execution of the check-package-manager.sh script
    In this commit, we did the following changes :
    1. Firstly, we added the feature of the user specific execution of the script. In this way, the script basically works like a module, which will be used in other scripts of this project. And, if the user wants the direct run of this script, then the user can use the below commands while calling the script on the terminal : ''bash check-package-manager.sh 0' or 'bash check-package-manager.sh true' or 'bash check-package-manager.sh execute'.
    2. Changed the variable name 'pmName' to 'packageManagerName' in order to make it more unique and distinguishable from other variables.

--- basic/check-package-manager.sh
+++ basic/check-package-manager.sh
@@ -1,19 +1,24 @@
+# ----
 # check-package-manager [bash / shell script]
 #
 # This script serves the function of checking the package-manager software that your current linux distro uses, and then gives the output to the user with all the required information. The script contains the functions and code that does it. The same code is used in the different scripts in this repository for checking the package-manager of the current linux distro.
 #
-# Author : Rishav Das (https://github.com/rdofficial)
+# Author : Rishav Das (https://github.com/rdofficial/)
 # Created on : April 31, 2021
 #
-# Last modified : -
-# Authored by : -
+# Last modified by : - Rishav Das (https://github.com/rdofficial/)
+# Last modified on : - May 3, 2021
+#
+# Changed made on last modification :
+# 1. Added the code for making the script more to work like a module, than an directly executable script. i.e., user now needs to enter some specific arguments while calling the script in order to run the script directly.
 #
 # Authors contributed to this script (Add your name below if you have contributed) :
 # 1. Rishav Das (https://github.com/rdofficial/, email:rdofficial192@gmail.com)
 #
+# ----
 
 # First checking for APT
-pmName=""
+packageManagerName=""
 if [[ -z $( type -p apt ) ]]; then
 	# If the package manager is not APT, then we continue for checking for pacman
 
@@ -34,28 +39,40 @@ if [[ -z $( type -p apt ) ]]; then
 				else
 					# If the package manager is ABS
 
-					pmName="abs"
+					packageManagerName="abs"
 				fi
 			else
 				# If the package manager is DNF
 
-				pmName="dnf"
+				packageManagerName="dnf"
 			fi
 		else
 			# If the package manager is YUM
 
-			pmName="yum"
+			packageManagerName="yum"
 		fi
 	else
 		# If the package manager is pacman
 
-		pmName="pacman"
+		packageManagerName="pacman"
 	fi
 else
 	# If the package manager is APT
 
-	pmName="apt"
+	packageManagerName="apt"
 fi
 
-# Finally displaying the name of the package manager on the console screen
-echo -e "The package manager used on this computer is : \033[92m${pmName}\033[00m"
+# ----
+# THE WAY OF DIRECTLY CALLING THE SCRIPT
+# 1. If the user wants the script to print the fetched information directly, then the user has to use the below specified commands to do so
+# 'bash check-package-manager.sh 0' or 'bash check-package-manager.sh true' or 'bash check-package-manager.sh execute'
+# 2. These steps are taken in such way, so that the script is used more as a module to load the required information stored variables into the other scripts of this project.
+# ----
+#
+# Checking the arguments entered by the user while calling this script
+if [[ $1 == "true" ]] || [[ $1 == "execute" ]] || [[ $1 == "0" ]]; then
+	# If the user entered the argument to directly execute the script, then we continue to do it
+
+	# Displaying the name of the package manager on the console screen
+	echo -e "The package manager used on this computer is : \033[92m${packageManagerName}\033[00m"
+fi
